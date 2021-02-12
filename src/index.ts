b1c654e0a584a1dcaa3b8bc7b1846a74d71ca75e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2021-02-12 21:30:26 +0100

    fix: prettier

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,12 @@ export default plugin(({ theme, addVariant, prefix, e: escape }) => {
             node.parent.parent.insertAfter(
               node.parent as Node,
               selectorParser().astSync(
-                `${prefix(`.group-${groupScope}:hover > .`)}${escape(`group-${groupScope}-hover${separator}${value}`)},
-                ${prefix(`.group-${groupScope}:hover :not(.group-${groupScope}) .`)}${escape(`group-${groupScope}-hover${separator}${value}`)}`
+                `${prefix(`.group-${groupScope}:hover > .`)}${escape(
+                  `group-${groupScope}-hover${separator}${value}`
+                )},
+                ${prefix(
+                  `.group-${groupScope}:hover :not(.group-${groupScope}) .`
+                )}${escape(`group-${groupScope}-hover${separator}${value}`)}`
               )
             );
           }
@@ -52,8 +56,12 @@ export default plugin(({ theme, addVariant, prefix, e: escape }) => {
             node.parent.parent.insertAfter(
               node.parent as Node,
               selectorParser().astSync(
-                `${prefix(`.group-${groupScope}:focus > .`)}${escape(`group-${groupScope}-focus${separator}${value}`)},
-                ${prefix(`.group-${groupScope}:focus :not(.group-${groupScope}) .`)}${escape(`group-${groupScope}-focus${separator}${value}`)}`
+                `${prefix(`.group-${groupScope}:focus > .`)}${escape(
+                  `group-${groupScope}-focus${separator}${value}`
+                )},
+                ${prefix(
+                  `.group-${groupScope}:focus :not(.group-${groupScope}) .`
+                )}${escape(`group-${groupScope}-focus${separator}${value}`)}`
               )
             );
           }
