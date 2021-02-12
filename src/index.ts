20027ef3b101bb87b1a5c0f66f01d5181afccc1b
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2021-02-12 20:36:54 +0100

    feat: implement nested groups plugin

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,66 @@
-export * from './lib/async';
-export * from './lib/number';
+import selectorParser from 'postcss-selector-parser';
+import plugin from 'tailwindcss/plugin';
+
+export default plugin(({ theme, addVariant, prefix, e: escape }) => {
+  const namedGroups = theme('namedGroups') || [];
+
+  addVariant(`group-hover`, ({ modifySelectors, separator }) => {
+    return modifySelectors(({ selector }) => {
+      return selectorParser((root) => {
+        root.walkClasses((node) => {
+          // Regular group
+          const value = node.value;
+          // eslint-disable-next-line functional/immutable-data
+          node.value = `group-hover${separator}${value}`;
+
+          node.parent.insertBefore(
+            node,
+            selectorParser().astSync(prefix(`.group:hover `))
+          );
+
+          // Named groups
+          node.parent.parent.insertAfter(
+            node.parent,
+            selectorParser().astSync(
+              prefix(`.group-scope:hover > .`) +
+                escape(`group-scope-hover${separator}${value}`) +
+                ',' +
+                prefix(`.group-scope:hover :not(.group-scope) .`) +
+                escape(`group-scope-hover${separator}${value}`)
+            )
+          );
+        });
+      }).processSync(selector);
+    });
+  });
+
+  addVariant(`group-focus`, ({ modifySelectors, separator }) => {
+    return modifySelectors(({ selector }) => {
+      return selectorParser((root) => {
+        root.walkClasses((node) => {
+          // Regular group
+          const value = node.value;
+          // eslint-disable-next-line functional/immutable-data
+          node.value = `group-focus${separator}${value}`;
+
+          node.parent.insertBefore(
+            node,
+            selectorParser().astSync(prefix(`.group:focus `))
+          );
+
+          // Named groups
+          node.parent.parent.insertAfter(
+            node.parent,
+            selectorParser().astSync(
+              prefix(`.group-scope:focus .`) +
+                escape(`group-scope-focus${separator}${value}`) +
+                ',' +
+                prefix(`.group-scope:focus :not(.group-scope) .`) +
+                escape(`group-scope-focus${separator}${value}`)
+            )
+          );
+        });
+      }).processSync(selector);
+    });
+  });
+});
